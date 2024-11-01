31908c014e37bb58b6f2be16cbb4e9e7adea3758
Alex Hardwick <AQ231458@aquinas.ac.uk>
2024-11-01 10:59:06 +0000

    message

--- MajorProject/Information.cs
+++ MajorProject/Information.cs
@@ -17,8 +17,9 @@ namespace MajorProject
 
         //static public SqlConnection SqlCon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Right Click\source\repos\MajorProjectRepo\MajorProject\AppData\Database.mdf;Integrated Security=True");
         //static public SqlConnection SqlCon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\alexh\source\repos\MajorProjectRepo\MajorProject\AppData\Database.mdf;Integrated Security = True");
-        
-        
+        static public SqlConnection SqlCon = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename=C:\Users\AQ231458\source\repos\AlexH231458\MajorProjectRepo\MajorProject\AppData\Database.mdf;Integrated Security = True");
+
+
         static public string userName;
         static public int userID;
         static public bool autoShift;
